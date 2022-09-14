38f85c6f57a47ea67b811a94cbe5dfe2c1dcfc28
sfatal1st <105299426+sfatal1st@users.noreply.github.com>
2022-09-14 16:22:18 +0300

    Add files via upload

--- public/loginPage.js
+++ public/loginPage.js
@@ -8,11 +8,11 @@ userForm.loginFormCallback = data => ApiConnector.login(data, response => {
     }
   });
 
-  userForm.registerFormCallback = data => ApiConnector.register(data, response => {
-    if (response["success"]) {
-        location.reload();
-    } else {
-        userForm.setLoginErrorMessage(response["error"]);
-    }
-  });
+userForm.registerFormCallback = data => ApiConnector.register(data, response => {
+  if (response["success"]) {
+      location.reload();
+  } else {
+      userForm.setLoginErrorMessage(response["error"]);
+  }
+});
 
